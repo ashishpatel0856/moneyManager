120cae487cd5f348a70865d29b16db18b45a006c
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-06 19:33:45 +0530

    categoryService

--- src/main/java/com/ashish/MoneyManager/controller/CategoryController.java
+++ src/main/java/com/ashish/MoneyManager/controller/CategoryController.java
@@ -1,14 +1,15 @@
 package com.ashish.MoneyManager.controller;
 
 import com.ashish.MoneyManager.dto.CategoryDto;
+import com.ashish.MoneyManager.dto.ProfileDto;
+import com.ashish.MoneyManager.entity.ProfileEntity;
 import com.ashish.MoneyManager.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -21,5 +22,24 @@ public class CategoryController {
     public ResponseEntity<CategoryDto> saveCategory(@RequestBody  CategoryDto categoryDto) {
         CategoryDto savedCategory = categoryService.saveCategory(categoryDto);
         return new ResponseEntity<>(savedCategory,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<CategoryDto>> getAllCategoriesByTypeForCurrentUser (@PathVariable String type) {
+        List<CategoryDto> categoryDtos = categoryService.getAllCategoriesByTypeForCurrentUser(type);
+        return new ResponseEntity<>(categoryDtos,HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getAllCategoriesForCurrentUser() {
+        List<CategoryDto> allCategories = categoryService.getAllCategoriesForCurrentUser();
+        return new ResponseEntity<>(allCategories,HttpStatus.OK);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDto> updateCategory(@PathVariable Long categoryId, @RequestBody CategoryDto categoryDto) {
+        CategoryDto savedCategory = categoryService.updateCategory(categoryId, categoryDto);
+        return new ResponseEntity<>(savedCategory,HttpStatus.OK);
+
     }
 }
--- src/main/java/com/ashish/MoneyManager/service/CategoryService.java
+++ src/main/java/com/ashish/MoneyManager/service/CategoryService.java
@@ -4,26 +4,63 @@ import com.ashish.MoneyManager.dto.CategoryDto;
 import com.ashish.MoneyManager.entity.CategoryEntity;
 import com.ashish.MoneyManager.entity.ProfileEntity;
 import com.ashish.MoneyManager.repository.CategoryRepository;
+import com.ashish.MoneyManager.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final AppUserDetailsService userDetailsService;
+    private final ProfileRepository profileRepository;
 
     public CategoryDto saveCategory(CategoryDto categoryDto) {
         ProfileEntity profile = userDetailsService.getCurrentProfile();
         if(categoryRepository.existsByNameAndProfileId(categoryDto.getName(),profile.getId())){
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category with this name already exists");
+            throw new RuntimeException("Category with this name already exists");
         }
         CategoryEntity newCategory =toEntity(categoryDto,profile);
         newCategory =categoryRepository.save(newCategory);
        return toDto(newCategory);
     }
+
+
+//  get category by type for current user
+    public List<CategoryDto> getAllCategoriesByTypeForCurrentUser  (String type) {
+        ProfileEntity profileEntity = userDetailsService.getCurrentProfile();
+        List<CategoryEntity> entities = categoryRepository.findByTypeAndProfileId(type,profileEntity.getId());
+        return entities
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    // get allCategory for current user
+    public List<CategoryDto> getAllCategoriesForCurrentUser(){
+        ProfileEntity profile = userDetailsService.getCurrentProfile();
+        List<CategoryEntity> categoryEntities = categoryRepository.findByProfileId(profile.getId());
+        return categoryEntities
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+
+    // update category by categoryId
+    public CategoryDto updateCategory(Long categoryId,CategoryDto categoryDto) {
+        ProfileEntity profile = userDetailsService.getCurrentProfile();
+        CategoryEntity existingCategory= categoryRepository.findByIdAndProfileId(categoryId,profile.getId())
+                .orElseThrow(() -> new RuntimeException("Category with this id does not exist"));
+        existingCategory.setName(categoryDto.getName());
+        existingCategory.setIcon(categoryDto.getIcon());
+        existingCategory= categoryRepository.save(existingCategory);
+        return toDto(existingCategory);
+    }
+
+
     public CategoryEntity toEntity(CategoryDto categoryDto, ProfileEntity profileEntity) {
         return CategoryEntity.builder()
                 .name(categoryDto.getName())
