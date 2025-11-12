eb53f97d22fab6d4de74961d625b1d8cd5eb2b71
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-12 17:33:16 +0530

    test

--- src/main/java/com/ashish/MoneyManager/MoneyManagerApplication.java
+++ src/main/java/com/ashish/MoneyManager/MoneyManagerApplication.java
@@ -3,14 +3,12 @@ package com.ashish.MoneyManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-@EnableScheduling
+
 @SpringBootApplication
 public class MoneyManagerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("SMTP Username: " + System.getenv("BREVO_USERNAME"));
-		System.out.println("SMTP Password: " + System.getenv("BREVO_PASSWORD"));
-		System.out.println("SMTP From: " + System.getenv("BREVO_FROM_EMAIL"));
+
 
 		SpringApplication.run(MoneyManagerApplication.class, args);
 	}
